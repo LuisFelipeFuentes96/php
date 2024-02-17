8bcbe64b73f7c2154899df0e73c4e2e330e86286
LuisFelipeFuentes96 <160342061+LuisFelipeFuentes96@users.noreply.github.com>
2024-02-17 13:03:55 -0500

    Add files via upload

---
+++ PDH05/semana2/ejercicio 1 intro php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Intro</title>
+</head>
+<body>
+    <h1>TALLER DE INTRODUCCION A PHP</h1>
+    <?php 
+    //Creación de variable en PHP
+    $nombre = "Luis Felipe Fuentes";
+    //para mostrar resultados por pantalla comando echo
+    echo "<p style='font-size:30px;color:blue;text-decotation:underline;'>Vamos a aprender PHP..".$nombre. "</p>";  
+    
+    ?>
+</body>
+</html>
---
+++ PDH05/semana2/inputprueba2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+    <h1>DATOS TRANSFERIDOS DEL FORMULARIO</h1>
+</body>
+</html>
+<?php 
+    //Viene de form login.php transferencia de datos
+echo "El usuario ingresado es...";
+echo "<br>";
+echo $_POST['username']; // Datos proviene del formulario
+
+
+?>
